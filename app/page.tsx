00f11dc32243bd2f58e293a27dd2cf1b18f19e76
Nick Echols <stere.ereo@gmail.com>
2023-12-21 01:44:25 -0800

    feat: cleanup loading state

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,17 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-12">
-      <UserImage />
-      <ApiData />
+    <main className="flex min-h-screen flex-col items-center justify-between p-12">
+      <div>
+        <UserImage />
+        <div
+          style={{ textWrap: "pretty" }} // TODO: no style attribute
+          className="flex flex-col items-center font-mono text-center my-16"
+        >
+          <ApiData />
+        </div>
+      </div>
+
       <UserButton />
     </main>
   );
