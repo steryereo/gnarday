55c36ac16f4c8ddd4bf103c02d52debe941d117e
Nick Echols <stere.ereo@gmail.com>
2024-09-02 22:45:00 -0700

    fix: upgrade packages and remove ignoreBuildErrors hack

--- next.config.js
+++ next.config.js
@@ -9,12 +9,6 @@ const nextConfig = {
       },
     ],
   },
-  typescript: {
-    // NOTE: currently next-auth's "auth" function does not pass build typechecking in route handlers
-    // see https://github.com/nextauthjs/next-auth-example/blob/main/app/api/protected/route.ts#L9
-    // TODO: remove this once next-auth gets its shit together, or maybe change to a different auth solution
-    ignoreBuildErrors: true,
-  },
 };
 
 module.exports = nextConfig;
